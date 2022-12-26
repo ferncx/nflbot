9fb95a562d7eb7d6e6f5caed2a0185ada2c899ba
jayleaf <rjayleaf@gmail.com>
2022-12-25 21:17:14 -0500

    update 1.1

--- commands/ask.js
+++ commands/ask.js
@@ -9,12 +9,12 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('ask')
         .setDescription('will answer your question about a player.')
-        .addStringOption(option => option.setName('sport').setRequired(true).setDescription('The sport your player plays for.').addChoice("NFL", "NFL").addChoice("NHL", "NHL").addChoice("NBA", "NBA").addChoice("MLB", "MLB").addChoice("PGA", "PGA"))
         .addStringOption(option => option.setName('question').setRequired(true).setDescription('The question you\'d like to ask.')),
 
     async execute(interaction) {
-        const query = interaction.options.getString('question').replace(' ', '-')
-        var url = `https://www.statmuse.com/${interaction.options.getString('sport')}/ask/${query}`
+        let atQ = interaction.options.getString('question').replace(' ', '-')
+        const query = `${atQ}-stats`
+        var url = `https://www.statmuse.com/ask/${query}`
         const data = await fetch(url)
         const rawHtml = await data.text()    
         const dom = new JSDOM(rawHtml)
@@ -27,11 +27,11 @@ module.exports = {
             if(similarity > 0.9) { // runs if the answer is similar enough to the question for the reasoning above
                 const image = dom.window.document.getElementsByClassName('h-44 md:h-52 self-center md:self-end mt-2 md:mt-0 md:pl-6 md:pr-1.5 select-none')[0].src // grabs the statmuse player image
                 const embed = new Discord.MessageEmbed()
-                    .setTitle(`Question: ${interaction.options.getString('question')}`)
+                    .setTitle(`Question: "*${interaction.options.getString('question')}*"`)
                     .setDescription('I didn\'t quite catch that; try rewording your question.')
                     .setThumbnail(image)
-                    .setFooter('Made by Jayleaf | Powered by Statmuse', 'https://www.statmuse.com/favicon.ico')
-                    .setAuthor(`Question asked by ${interaction.user.username}`, interaction.user.displayAvatarURL())
+                    .setFooter({text: 'Made by Jayleaf | Powered by Statmuse', iconURL: 'https://i.imgur.com/uENX5KO.jpg' })
+                    .setAuthor({name: `Question asked by ${interaction.member.nickname ? interaction.member.nickname : interaction.user.username}`, iconURL: interaction.user.displayAvatarURL()})
                     .setColor('#0099ff')
                 interaction.reply({embeds: [embed]})
                 return;
@@ -39,11 +39,11 @@ module.exports = {
             // normal function; runs if the answer is not too similar to the question
             const image = dom.window.document.getElementsByClassName('h-44 md:h-52 self-center md:self-end mt-2 md:mt-0 md:pl-6 md:pr-1.5 select-none')[0].src
             const embed = new Discord.MessageEmbed()
-                .setTitle(`Question: ${interaction.options.getString('question')}`)
+            .setTitle(`Question: "*${interaction.options.getString('question')}*"`)
                 .setDescription(answer)
                 .setThumbnail(image)
-                .setFooter('Made by Jayleaf | Powered by Statmuse', 'https://www.statmuse.com/favicon.ico')
-                .setAuthor(`Question asked by ${interaction.user.username}`, interaction.user.displayAvatarURL())
+                .setFooter({text: 'Made by Jayleaf | Powered by Statmuse', iconURL: 'https://i.imgur.com/uENX5KO.jpg' })
+                .setAuthor({name: `Question asked by ${interaction.member.nickname ? interaction.member.nickname : interaction.user.username}`, iconURL: interaction.user.displayAvatarURL()})
                 .setColor('#0099ff')
             interaction.reply({embeds: [embed]})
             return;
@@ -53,26 +53,17 @@ module.exports = {
                 const answer = dom.window.document.getElementsByClassName('nlg-answer')[0].textContent
                 const image = dom.window.document.getElementsByClassName('h-44 md:h-52 self-center md:self-end mt-2 md:mt-0 md:pl-6 md:pr-1.5 select-none')[0].src
                 const embed = new Discord.MessageEmbed()
-                    .setTitle(`Question: ${interaction.options.getString('question')}`)
+                    .setTitle(`Question: "*${interaction.options.getString('question')}*"`)
                     .setDescription(answer)
                     .setThumbnail(image)
-                    .setFooter('Made by Jayleaf | Powered by Statmuse', 'https://www.statmuse.com/favicon.ico')
-                    .setAuthor(`Question asked by ${interaction.user.username}`, interaction.user.displayAvatarURL())
+                    .setFooter({text: 'Made by Jayleaf | Powered by Statmuse', iconURL: 'https://i.imgur.com/uENX5KO.jpg' })
+                    .setAuthor({name: `Question asked by ${interaction.member.nickname ? interaction.member.nickname : interaction.user.username}`, iconURL: interaction.user.displayAvatarURL()})
                     .setColor('#0099ff')
                 interaction.reply({embeds: [embed]})
                 return;
             } catch(e) {
-                // This should never run, and if it does, people should report it to me
-                interaction.reply({content: "An internal error occurred.", ephemeral: true})
+                
             }
-        }
-        
-            
-       
-        
-
-        
-           
-       
+        } 
     }
 }
