20b3670129caad084738a3d83d49174610cb9a60
jayleaf <rjayleaf@gmail.com>
2023-02-27 19:31:11 -0500

    made it so if you type stats, it won't auto-append it to your question

--- commands/ask.js
+++ commands/ask.js
@@ -13,7 +13,12 @@ module.exports = {
 
     async execute(interaction) {
         let atQ = interaction.options.getString('question').replace(' ', '-')
-        const query = `${atQ}-stats`
+        var query;
+        if(interaction.options.getString('question').includes("stats")) {
+            query = `${atQ}-stats`
+        } else {
+            query = `${atQ}`
+        }
         var url = `https://www.statmuse.com/ask/${query}`
         const data = await fetch(url)
         const rawHtml = await data.text()    
