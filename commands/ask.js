d6e1bc737be73719dcec6b2867c07c0d2bb90293
jayleaf <rjayleaf@gmail.com>
2023-02-27 19:55:55 -0500

    pls

--- commands/ask.js
+++ commands/ask.js
@@ -13,8 +13,8 @@ module.exports = {
 
     async execute(interaction) {
         let atQ = interaction.options.getString('question').replace(' ', '-')
-        var query;
-        atQ.includes("stats") ? query = `${atQ}`.replace(' ', '') : query = `${atQ}-stats`.replace(' ', '')
+        var query = atQ.includes("stats") ? atQ.replace(' ', '') : `${atQ}-stats`.replace(' ', '')
+        console.log(query)
         const url = `https://www.statmuse.com/ask/${query}`
         const data = await fetch(url)
         const rawHtml = await data.text()    
